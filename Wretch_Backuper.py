e4c6e4b5974470b3b80488daefeb7186e93d7d16
owl3808 <owl3808@gmail.com>
2013-11-29 23:52:45 +0800

    change encoding to UTF-8

--- Wretch_Backuper.py
+++ Wretch_Backuper.py
@@ -3,8 +3,17 @@ from HTMLParser import HTMLParser
 import codecs
 import os
 import urllib2
-#import pdb
-#pdb.set_trace()
+
+# setting default encoding
+'''default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+''''''
+# debugger
+import pdb
+pdb.set_trace()
+'''
 
 # create a subclass and override the handler methods
 class GetDetailInfo(HTMLParser):
@@ -15,23 +24,23 @@ class GetDetailInfo(HTMLParser):
     filename=""
     def handle_data(self, data):
         if self.is_table_found:
-            if data == u'相簿標題:':
+            if data == '相簿標題:':
                 self.is_title_found=True
                 return
         
             if self.is_title_found:
                 self.name = data
-                #print u'相簿標題:'+ self.name
+                #print '相簿標題:'+ self.name
                 self.is_title_found=False
              
-            if data == u'內容連結檔案:':
+            if data == '內容連結檔案:':
                 self.is_filename_found=True
                 return
         
     def handle_starttag(self, tag, attrs):
         if tag=='a' and self.is_filename_found:
             self.filename = attrs[0][1]
-            #print u"檔案:"+ str(self.filename)
+            #print "檔案:"+ str(self.filename)
             self.is_filename_found=False
         if tag == 'table':
             self.is_table_found=True
@@ -49,19 +58,23 @@ def downfile(url, destdir):
             code.write(f.read())
     except urllib2.HTTPError, err:
         if err.code == 404:
-            print u'下載"%s"失敗'%filename
+            print '下載"%s"失敗'%filename
     except ValueError:
-        print u'url為"%s"無法存取'%url
+        print 'url為"%s"無法存取'%url
         
+
 
 
     
 # read
-#dest=input('請輸入目地位置,(default:"./")')
-dest=u'./album'
+try:
+    CWD= os.path.dirname(os.path.abspath(__file__)).replace('\\','/')
+    dest=CWD + '/album'
+except NameError:
+    dest='./album'
           
 # get the file of album detail ( title and descripts )
-fileopen = codecs.open(dest+'/detail.html','r','utf-8')
+fileopen = open(dest+'/detail.html','rb')
 details = fileopen.read()
 fileopen.close()
 
@@ -79,31 +92,32 @@ for Album in Albums:
     # setting paramaters
     name=Album['name']
     # remove char illegal for dirname
-    name=name.replace('/','').replace(u'"','').replace(u'?','').replace(u':','').replace(u'*','').replace(u'|','').replace(u'<','').replace(u'>','')
+    name=name.replace('/','').replace('"','').replace('?','').replace(':','').replace('*','').replace('|','').replace('<','').replace('>','')
     urlfile=Album['filename']
-    albumpath= dest + u'/' + name
-    print u''
-    print u'開始備份: %s'%name
+    albumpath= dest + '/' + name
+    print ''
+    print '開始備份: %s'%name
 
     # make dir
     try:
-        os.mkdir(albumpath)
-    except WindowsError:
-        print u"Error: 建立「%s」資料夾失敗，可能是資料夾已經存在，此相簿將放棄備份"%name
+        os.mkdir(albumpath.decode('utf-8'))
+    except WindowsError as e:
+        print "Error: 建立「%s」資料夾失敗，可能是資料夾已經存在，此相簿將放棄備份"%name
+        print e
         continue
 
     # read url file
     try:
         f= codecs.open(dest+'/'+urlfile,'r','utf-8')
     except IOError:
-        print u"Error: 無法找到關聯的URL檔案:%s , 此相簿將放棄備份"
+        print "Error: 無法找到關聯的URL檔案:%s , 此相簿將放棄備份"%urlfile
         continue
-        
-    print urlfile
-    for url in f.readlines():
-        downfile(url,albumpath)
 
-    print u'完成'
+    # download each file
+    for url in f.readlines():
+        downfile(url.decode('utf-8') ,albumpath.decode('utf-8'))
+
+    print '完成'
     
     
     
